372250b8badc3ed6001ddc6d5fa6d3e621df4401
SlongLiu <slongliu86@gmail.com>
2022-04-10 16:23:19 +0800

    public version

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+# ------------------------------------------------------------------------
+# DAB-DETR
+# Copyright (c) 2022 IDEA. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+
+from .DAB_DETR import build_DABDETR
+from .dab_deformable_detr import build_dab_deformable_detr
