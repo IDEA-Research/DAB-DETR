411d8c6e9a68b0e6fb6d1308158396eccfc142b1
William Justin <williamjustinxu@gmail.com>
2022-04-19 12:25:35 +0700

    Fix structure

--- coco.sh
+++ coco.sh
@@ -1,21 +1,18 @@
 mkdir COCODIR
 cd COCODIR
 
-mkdir train2017
-cd train2017
 wget -c http://images.cocodataset.org/zips/train2017.zip
+echo "Extracting train2017.zip"
 unzip -qq train2017.zip
 rm train2017.zip
 
-cd ..
-mkdir val2017
-cd val2017
 wget -c http://images.cocodataset.org/zips/val2017.zip
+echo "Extracting val2017.zip"
 unzip -qq val2017.zip
 rm val2017.zip
 
-cd ..
 wget -c http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+echo "Extracting annotations_trainval2017.zip"
 unzip -qq annotations_trainval2017.zip
 rm annotations_trainval2017.zip
 
